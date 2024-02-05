eff7b692a2963e097859b65fbac1b60eb0ad2f06
Siarhei Bakatsiuk <s@bokatuk.com>
2024-02-05 16:22:29 +0300

    add retargeting script

--- target.sh
+++ target.sh
@@ -5,13 +5,17 @@ else
 sed -E -i "" "s/<TargetFramework>net([0-9]{1,}\.)+[0-9]{1,}-ios/<TargetFramework>net$1-ios/" Bindings/OpenTok.Net.iOS/OpenTok.Net.iOS.csproj
 sed -E -i "" "s/<TargetFramework>net([0-9]{1,}\.)+[0-9]{1,}-android/<TargetFramework>net$1-android/" Bindings/OpenTok.Net.Android/OpenTok.Net.Android.csproj
 
-# if [[ "$1" == "7.0" ]]
-# then
-#net7.0-android33.0
-#targetFramework="
-#sed -E -i "" "s/<TargetFramework>net([0-9]{1,}\.)+[0-9]{1,}-ios/<TargetFramework>net$1-ios/" NugetPackages/OpenTok.Net.nuspec
-#net8.0-android34.0
-# fi
+if [[ "$1" == "7.0" ]]
+then
+rm NugetPackages/OpenTok.Net.nuspec
+cp -R NugetPackages/OpenTok.Net.nuspec.7.0 NugetPackages/OpenTok.Net.nuspec
+fi
+
+if [[ "$1" == "8.0" ]]
+then
+rm NugetPackages/OpenTok.Net.nuspec
+cp -R NugetPackages/OpenTok.Net.nuspec.8.0 NugetPackages/OpenTok.Net.nuspec
+fi
 
 echo "Target for Opentok set to $1"
 fi
